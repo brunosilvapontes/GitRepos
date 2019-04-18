c3a07ab466f4c2be0f5380402204e023c1425606
Bruno Silva Pontes <brunospontes@hotmail.com>
2019-04-18 08:29:07 -0300

    Removed redundant code

--- routes/github.js
+++ routes/github.js
@@ -1,8 +1,7 @@
 const router = require('express').Router()
 const controller = require('../controllers/github')
-const auth = require('./auth')
 
 // Get some github repositories' (not forks) data from a given username
-router.get('/:username', auth.checkAcceptHeader, controller.getRepositories)
+router.get('/:username', controller.getRepositories)
 
 module.exports = router
